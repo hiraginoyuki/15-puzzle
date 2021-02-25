ef8b11b739e91d12c8985ec96b2783b5064c29b4
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-02-25 05:08:58 -0800

    Add 15-puzzle

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { FifteenPuzzle } from './15-puzzle';
---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export const chooseRandomIndex = <T>(array: T[]): number => Math.floor(Math.random() * array.length);
+export const chooseRandom = <T>(array: T[]): T => array[chooseRandomIndex(array)];
+
+export const flip = <T>(e: T, i: number, a: T[]) => a[a.length - 1 - i];
+
+export function range(end: number): number[];
+export function range(start: number, end: number): number[];
+export function range(start: number, end: number, step: number): number[];
+export function range(...args: number[]): number[] {
+  switch (args.length) {
+    case 1:
+      return range(0, args[0], 1);
+    case 2:
+      return range(args[0], args[1], 1);
+    default:
+      const [start, end, step] = args;
+      return [...Array(Math.ceil((end - start) / step))].map((v: number, i: number) => start + i * step);
+  }
+}
